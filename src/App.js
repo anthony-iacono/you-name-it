9b9639e7572a055c05dae0909fce5f596080b620
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-11-10 11:56:47 -0800

    Add Flow

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+// @flow
+import * as React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(): React.Node {
   return (
     <div className="App">
       <header className="App-header">
