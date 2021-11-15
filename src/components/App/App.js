78eca2f83074f26578ae403fc64fdd6fc237afd5
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-11-14 21:58:07 -0800

    Control Splitter

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
-// @flow
 import * as React from 'react';
 import './App.css';
 import Header from '../Header/Header';
+import Splitter from '../Splitter/Splitter';
 
 function App(): React.Node {
+  const [keywords, setKeywords] = React.useState('');
   return (
     <div className="App">
       <Header />
+      { !keywords && <Splitter handleSubmit={setKeywords} /> }
     </div>
   );
 }
---
+++ src/components/Splitter/Splitter.js
@@ -0,0 +1,26 @@
+// @flow
+import { useState } from 'react';
+import './Splitter.css';
+
+type SplitterProps = {
+  handleSubmit: (string) => void;
+}
+
+const Splitter = ({ handleSubmit }: SplitterProps) => {
+  const [keywords, setKeywords] = useState('');
+  return (
+    <>
+      <form onSubmit={() => handleSubmit(keywords)}>
+        <input
+          type="text"
+          placeholder="keywords"
+          value={keywords}
+          onChange={(e) => setKeywords(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+};
+
+export default Splitter;
