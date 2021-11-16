0b9997bde6bf5e1f6dc186d1168de860c1444aeb
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-11-16 10:52:51 -0800

    Flesh out Favorites

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,5 +1,22 @@
+import { Link } from 'react-router-dom';
 import './Favorites.css';
 
-export default function Favorites({ favorites }) {
+type FavoritesProps = {
+  favorites: string[],
+}
 
+export default function Favorites({ favorites }: FavoritesProps) {
+  return (
+    <div>
+      <h2>Favorites</h2>
+      { !favorites && (
+        <>
+          <p>Go back and add some favorites!</p>
+          <Link to="/results">
+            <button type="button">Back</button>
+          </Link>
+        </>
+      )}
+    </div>
+  );
 }
